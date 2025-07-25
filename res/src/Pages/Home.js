0c68918eafcde8c43a2ed82caf98ed060c68decb
HumungoBear12 <amakris03@gmail.com>
2025-07-24 17:22:31 -0700

    Push it

--- res/src/Pages/Home.js
+++ res/src/Pages/Home.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import Nav from '../Components/NavBar'
-import Learning from '../Components/Learning'
-import Problem from '../Components/Problem'
-import Professional from '../Components/Professional'
-import Projects from '../Components/Projects'
-import Skills from '../Components/Skills' 
-import Footer from '../Components/Footer'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from '../Components/NavBar';
+import Professional from '../Components/Professional';
+import Skills from '../Components/Skills';
+import Projects from '../Components/Projects';
+import Problem from '../Components/Problem';
+import Learning from '../Components/Learning';
 import Banner from '../Components/Banner'
+import Footer from '../Components/Footer';
 
 const Home = () => {
   return (
     <div>
-        <Nav/>
-        <Banner/>
-        <Professional/>
-        <Skills/>
-        <Projects/>
-        <Problem/>
-        <Learning/>
-        <Footer/>
+      <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/banner" element={<Banner />} />
+        <Route path="/experience" element={<Professional />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/problem-solving" element={<Problem />} />
+        <Route path="/learning" element={<Learning />} />
+        <Route path="/" element={<Professional />} /> {/* Default route */}
+      </Routes>
+    </Router>
+    <Footer/>
     </div>
-  )
-}
+    
+  );
+};
 
-export default Home
+export default Home;
