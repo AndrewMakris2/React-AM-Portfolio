ceeee9bdb66934f6b78e50ffb65269b25ad6cb0a
HumungoBear12 <amakris03@gmail.com>
2025-09-05 11:27:40 -0700

    push

--- res/src/Components/Projects.jsx
+++ res/src/Components/Projects.jsx
@@ -5,29 +5,29 @@ const Projects = () => {
     <div className="Projects">
       <h2>My Projects</h2>
       <hr />
-      <div className="Project">
+      <div className="Project"> 
         <div>
           <h3>Python Projects</h3>
           <ul>
-            <li><a href="https://github.com/AndrewMakris2/Python-vpn">VPN Project (Python Code)</a></li>
-            <li><a href="https://github.com/AndrewMakris2/Python-Log-Analyzer">Log Analyzer (Python Code)</a></li>
-            <li><a href="#">Project Name 3</a></li>
+            <li><a href="https://github.com/AndrewMakris2/Python-vpn" target="_blank" rel="noreferrer">VPN Project (Python Code)</a></li>
+            <li><a href="https://github.com/AndrewMakris2/Python-Log-Analyzer" target="_blank" rel="noreferrer">Log Analyzer (Python Code)</a></li>
+            <li><a href="https://github.com/AndrewMakris2/Python-IOCAnalyzer" target="_blank" rel="noreferrer">IOC Analyzer (Python Code)</a></li>
           </ul>
         </div>
         <div>
           <h3>React Projects</h3>
           <ul>
-            <li><a href="https://fancy-pastelito-fa9d8c.netlify.app/">VPN Project (React Webiste)</a></li>
-            <li><a href="https://eloquent-babbage-15108c.netlify.app/">Old Portfolio (2021)</a></li>
-            <li><a href="https://suspicious-pike-55fa2c.netlify.app/">Batman (Internal APIs)</a></li>
+            <li><a href="https://fancy-pastelito-fa9d8c.netlify.app/" target="_blank" rel="noreferrer">VPN Project (React Webiste)</a></li>
+            <li><a href="https://eloquent-babbage-15108c.netlify.app/" target="_blank" rel="noreferrer">Old Portfolio (2021)</a></li>
+            <li><a href="https://suspicious-pike-55fa2c.netlify.app/" target="_blank" rel="noreferrer">Batman (Internal APIs)</a></li>
           </ul>
         </div>
         <div>
-          <h3>Miscelanious Projects</h3>
+          <h3>Miscellaneous Projects</h3>
           <ul>
-            <li><a href="https://github.com/Amakris12/Store-Shoe-API">External API Project</a></li>
-            <li><a href="https://github.com/Amakris12/Nodejs-Passport-Login">Secure Web Authentication with Passport.js</a></li>
-            <li><a href="https://github.com/Amakris12/EJS-Login">EJS Login</a></li>
+            <li><a href="https://github.com/Amakris12/Store-Shoe-API" target="_blank" rel="noreferrer">External API Project</a></li>
+            <li><a href="https://github.com/Amakris12/Nodejs-Passport-Login" target="_blank" rel="noreferrer">Secure Web Authentication with Passport.js</a></li>
+            <li><a href="https://github.com/Amakris12/EJS-Login" target="_blank" rel="noreferrer">EJS Login</a></li>
           </ul>
         </div>
       </div>
@@ -37,7 +37,7 @@ const Projects = () => {
         <div className="MoreProjects">
           <div>
             <h4>GitHub Profile</h4>
-            <a href="https://github.com/AndrewMakris2" target="_blank">
+            <a href="https://github.com/AndrewMakris2" target="_blank" rel="noreferrer">
               <img
                 src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
                 alt="GitHub"
@@ -46,7 +46,7 @@ const Projects = () => {
           </div>
           <div>
             <h4>LinkedIn Profile</h4>
-            <a href="https://www.linkedin.com/in/andrew-makris-441969258/" target="_blank">
+            <a href="https://www.linkedin.com/in/andrew-makris-441969258/" target="_blank" rel="noreferrer">
               <img
                 src="https://images.vexels.com/media/users/3/137382/isolated/preview/c59b2807ea44f0d70f41ca73c61d281d-linkedin-icon-logo.png"
                 alt="LinkedIn"
